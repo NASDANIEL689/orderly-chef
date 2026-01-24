ea98631a3133a03be7c366622247a5c57645b84d
NASDANIEL689 <CSE24-040@thuto.bac.ac.bw>
2026-01-24 16:05:16 +0200

    configure site to work one last time

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_URL || "/",
+  base: process.env.VITE_BASE_URL || "/orderly-chef/",
   server: {
     host: "::",
     port: 8080,
